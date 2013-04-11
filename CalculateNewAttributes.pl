ba87be9ba7ef5deb473221b361edbdfa5d30a98b
Greg Mojonnier <gregmojonnier@gmail.com>
2013-04-11 18:54:24 -0400

    Begun working on main loop to calculate new attributes for each line

--- CalculateNewAttributes.pl
+++ CalculateNewAttributes.pl
@@ -11,6 +11,44 @@ chomp $columnHeaders[ $#columnHeaders ];
 
 my %attributesIndex;
 
+# Map all the attribute indexes by their abbreviation
+# to the index their located at for make our calculations more readable
 for my $index( 0..$#columnHeaders ){
 	$attributesIndex{ $columnHeaders[ $index ] } = $index;
+}
+
+my $totalSeasonMinutes = 48 * 82;
+
+# For each line, calculate the new attributes,
+# append them to the original line, then overwrite the old line in @allFileLines
+for my $lineNum( 1..$#allFileLines ){
+	my $curLine = $allFileLines[ $lineNum ];
+
+	if( length chomp $curLine ){
+		my @curLineAttributes = split /,/, $curLine;
+		# For each line(player), will be adding the following attributes
+		# MPPS -- Minutes Played Per Seaseon
+		#      -> ( total minutes / (48*82) )
+
+		# OEPM -- Offensive Effiency Per Minute 
+		#      -> ( (total points + 2.25 * assists + free throws made - turnovers, / total minutes) * MPPS )
+
+		# DEPM -- Defensive Effiency Per Minute 
+		#      -> ( (rebounds + 1.5 * steals + 1.25 * blocks / total minutes) * MPPS )
+
+
+		# Get each value we need from appropriate index
+		my $totalMinutes = $curLineAttributes[ $attributesIndex{ "Min" } ];
+
+		my $pts = $curLineAttributes[ $attributesIndex{ "Pts" } ];
+		my $assists = $curLineAttributes[ $attributesIndex{ "AST" } ];
+		my $ftm = $curLineAttributes[ $attributesIndex{ "FTM" } ];
+		my $turnovers = $curLineAttributes[ $attributesIndex{ "TO" } ];
+
+		my $rebounds = $curLineAttributes[ $attributesIndex{ "REB" } ];
+		my $steals = $curLineAttributes[ $attributesIndex{ "STL" } ];
+		my $blocks = $curLineAttributes[ $attributesIndex{ "BLK" } ];
+
+
+	}
 }
